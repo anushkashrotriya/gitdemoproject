a27786e574f7284445f758c2af1d594fe54653df
thesa <thesa@DESKTOP-28CKLHF>
2022-10-18 20:39:00 -0400

    First commit.....

---
+++ src/Testgitrepo/Test1.java
@@ -0,0 +1,24 @@
+package Testgitrepo;
+
+public class Test1 {
+	
+	public void git() {
+		
+		System.out.println("first git demo program");
+	}
+	
+	public void git1() {
+		
+		System.out.println("success");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Test1 test = new Test1();
+		test.git();
+		test.git1();
+
+	}
+
+}
